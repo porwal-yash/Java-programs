264053882f31a739821bbcf012e82dd2616d5632
shanky3939 <83959915+shanky3939@users.noreply.github.com>
2021-10-28 11:54:42 +0530

    code  has been completely changed for betterment

--- code.java
+++ code.java
@@ -1,20 +1,20 @@
-//
+
 //call by reference or object reference
 
-class obj_ref
-{
-int a=10;
-int b=20;
-  void swap(int x,int y)   
-  {
-    x = x + y;
-    y = x - y;
-    x = x - y;
-   }
-   public static void main(String[] a1) {
-        obj_ref o1=new obj_ref();
-        o1.swap(o1.a,o1.b);   
-        System.out.println("A ="+o1.a);
-        System.out.println("B ="+o1.b);
-     }
-}
+class Operation2{  
+ int data=50;  
+  
+ void change(Operation2 op){  
+ op.data=op.data+100;//changes will be in the instance variable  
+ }  
+     
+    
+ public static void main(String args[]){  
+   Operation2 op=new Operation2();  
+  
+   System.out.println("before change "+op.data);  
+   op.change(op);//passing object  
+   System.out.println("after change "+op.data);  
+  
+ }  
+}  
