32c150c19c4de33be99c0496221c89307bac6ecc
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-10-28 18:25:44 +0530

    Improved code3.java

--- code3.java
+++ code3.java
@@ -1,13 +1,11 @@
-
-class code3{
-
-public static void main(String[] args){
-  int n=123;
-  int last;
-  for(int i=0;n!=0;i++){
-    last=n%10;
-    n=n/10;
-    System.out.print(last);
-  }
-}
+public class code3{
+  public static void main(String[] args){
+      int n=123;
+      int last;
+      for(int i=0;n!=0;i++){
+          last=n%10;
+          n=n/10;
+          System.out.print(last);
+     }
+   }
 }
